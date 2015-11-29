c4e0e7bb734e57cdf126bd6c63c9439ed25d270e
aktse <akt@ualberta.ca>
2015-11-28 22:25:37 -0700

    removed some junk

--- UAlbertaBot/Source/StrategyManager.cpp
+++ UAlbertaBot/Source/StrategyManager.cpp
@@ -173,12 +173,7 @@ const MetaPairVector StrategyManager::getProtossBuildOrderGoal() const
 	{
 		if (numZealots == 0)
 		{
-			goal.push_back(MetaPair(BWAPI::UnitTypes::Protoss_Zealot, numZealots + 2));
 			goal.push_back(MetaPair(BWAPI::UnitTypes::Protoss_Shuttle, 1));
-		}
-		else
-		{
-			goal.push_back(MetaPair(BWAPI::UnitTypes::Protoss_Zealot, numZealots + 2));
 		}
 
 		// Units
